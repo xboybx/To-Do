dee9d3089003557287d4f57318adee2f2578aa14
xboybx <146360441+xboybx@users.noreply.github.com>
2023-10-13 15:52:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,159 +1,4 @@
-// // Initialize the to-do list from local storage or use an empty array
-// const Todo_list = JSON.parse(localStorage.getItem('todoList')) || [];
-
-// rendertodolist();
-
-// function rendertodolist() {
-//     let final_todo_list = '';
-//     const noTasksMessage = document.getElementById('no-tasks-message');
-
-//     if (Todo_list.length === 0) {
-//         noTasksMessage.textContent = "No tasks are available.";
-//     } else {
-//         noTasksMessage.textContent = "";
-
-//         for (let i = 0; i < Todo_list.length; i++) {
-//             const looped_resultobj = Todo_list[i];
-//             const todotask = looped_resultobj.task_list;
-//             const todo_date = looped_resultobj.datee;
-
-//             let html_code = `
-//                 <div>${todotask}</div>
-//                 <div>${todo_date}</div>
-//                 <i class="fa-regular fa-star important-button" id="important-button"></i>
-//                 <button class="edit-button">Edit</button>
-//                 <button class='delet_button JS-delete-todo-button' data-index="${i}">Delete</button>
-//                 `;
-//             final_todo_list += html_code;
-//         }
-//     }
-
-//     document.querySelector('.display_result').innerHTML = final_todo_list;
-
-//     document.querySelectorAll('.JS-delete-todo-button')
-//         .forEach((delete_button) => {
-//             delete_button.addEventListener('click', (event) => {
-//                 const indexToDelete = event.target.getAttribute('data-index');
-//                 if (indexToDelete !== null) {
-//                     Todo_list.splice(indexToDelete, 1);
-//                     saveTodoListToLocalStorage();
-//                     rendertodolist();
-//                 }
-//             });
-//         });
-
-//     addEditListeners(); // Add edit listeners when rendering
-// }
-
-// function showWarning(message) {
-//     const warningMessage = document.getElementById('warning-message');
-//     warningMessage.textContent = message;
-// }
-
-// function clearWarning() {
-//     const warningMessage = document.getElementById('warning-message');
-//     warningMessage.textContent = "";
-// }
-
-// function adding_the_todolist() {
-//     const entering_todolist = document.querySelector('.inputed_value');
-//     const todo_list_entered = entering_todolist.value;
-//     const date_get_element = document.querySelector('.date_selector');
-//     const date_store_vari = date_get_element.value;
-//     const noTasksMessage = document.getElementById('no-tasks-message');
-
-//     if (todo_list_entered.trim() === '') {
-//         showWarning("Fill the task");
-//     } else {
-//         clearWarning();
-//         Todo_list.push({
-//             task_list: todo_list_entered,
-//             datee: date_store_vari
-//         });
-
-//         entering_todolist.value = '';
-
-//         saveTodoListToLocalStorage();
-//         rendertodolist();
-
-//         noTasksMessage.textContent = "";
-//     }
-// }
-
-// function saveTodoListToLocalStorage() {
-//     localStorage.setItem('todoList', JSON.stringify(Todo_list));
-// }
-
-// document.querySelector('.js-add-todo-button')
-//     .addEventListener('click', () => {
-//         adding_the_todolist();
-//     });
-
-// document.querySelector('.inputed_value')
-//     .addEventListener('keydown', (event) => {
-//         if (event.key === 'Enter') {
-//             adding_the_todolist();
-//         }
-//     });
-
-// function addEditListeners() {
-//     const editButtons = document.querySelectorAll('.edit-button');
-
-//     editButtons.forEach((editButton, index) => {
-//         editButton.addEventListener('click', () => {
-//             // Get the corresponding task div
-//             const taskDiv = document.querySelectorAll('.display_result div')[index * 2];
-
-//             // Get the current task text
-//             const currentTaskText = taskDiv.textContent;
-
-//             // Create an input field for editing
-//             const inputField = document.createElement('input');
-//             inputField.value = currentTaskText;
-
-//             // Replace the task text with the input field
-//             taskDiv.textContent = '';
-//             taskDiv.appendChild(inputField);
-
-//             // Create a "Save" button
-//             const saveButton = document.createElement('button');
-//             saveButton.textContent = 'Save';
-
-//             // Add a click event for saving the edited task
-//             saveButton.addEventListener('click', () => {
-//                 const editedText = inputField.value;
-
-//                 // Update the task text and render the updated list
-//                 Todo_list[index].task_list = editedText;
-//                 saveTodoListToLocalStorage();
-//                 rendertodolist();
-//             });
-
-//             // Add the "Save" button after the input field
-//             taskDiv.appendChild(saveButton);
-//             saveButton.style.color = "#7fbc8c";
-//             saveButton.style.fontWeight = "700";
-//             saveButton.style.fontSize = "20px";
-//             saveButton.style.border = "none";
-//         });
-//     });
-// }
-
-// // imp code
-
-// document.getElementById('important-button').addEventListener('click', () => {
-
-//     const textElements = document.querySelectorAll('.display_result div:first-child');
-//     textElements.forEach((element) => {
-//         element.classList.toggle('important-text');
-
-//     });
-// });
-
-
 // STAR BUTTON FUNCTIONALITY IMPLEMENTED
-
-
 
 // Initialize the to-do list from local storage or use an empty array
 const Todo_list = JSON.parse(localStorage.getItem('todoList')) || [];
@@ -326,3 +171,157 @@ function addImportantListeners() {
         });
     });
 }
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+// // Initialize the to-do list from local storage or use an empty array
+// const Todo_list = JSON.parse(localStorage.getItem('todoList')) || [];
+
+// rendertodolist();
+
+// function rendertodolist() {
+//     let final_todo_list = '';
+//     const noTasksMessage = document.getElementById('no-tasks-message');
+
+//     if (Todo_list.length === 0) {
+//         noTasksMessage.textContent = "No tasks are available.";
+//     } else {
+//         noTasksMessage.textContent = "";
+
+//         for (let i = 0; i < Todo_list.length; i++) {
+//             const looped_resultobj = Todo_list[i];
+//             const todotask = looped_resultobj.task_list;
+//             const todo_date = looped_resultobj.datee;
+
+//             let html_code = `
+//                 <div>${todotask}</div>
+//                 <div>${todo_date}</div>
+//                 <i class="fa-regular fa-star important-button" id="important-button"></i>
+//                 <button class="edit-button">Edit</button>
+//                 <button class='delet_button JS-delete-todo-button' data-index="${i}">Delete</button>
+//                 `;
+//             final_todo_list += html_code;
+//         }
+//     }
+
+//     document.querySelector('.display_result').innerHTML = final_todo_list;
+
+//     document.querySelectorAll('.JS-delete-todo-button')
+//         .forEach((delete_button) => {
+//             delete_button.addEventListener('click', (event) => {
+//                 const indexToDelete = event.target.getAttribute('data-index');
+//                 if (indexToDelete !== null) {
+//                     Todo_list.splice(indexToDelete, 1);
+//                     saveTodoListToLocalStorage();
+//                     rendertodolist();
+//                 }
+//             });
+//         });
+
+//     addEditListeners(); // Add edit listeners when rendering
+// }
+
+// function showWarning(message) {
+//     const warningMessage = document.getElementById('warning-message');
+//     warningMessage.textContent = message;
+// }
+
+// function clearWarning() {
+//     const warningMessage = document.getElementById('warning-message');
+//     warningMessage.textContent = "";
+// }
+
+// function adding_the_todolist() {
+//     const entering_todolist = document.querySelector('.inputed_value');
+//     const todo_list_entered = entering_todolist.value;
+//     const date_get_element = document.querySelector('.date_selector');
+//     const date_store_vari = date_get_element.value;
+//     const noTasksMessage = document.getElementById('no-tasks-message');
+
+//     if (todo_list_entered.trim() === '') {
+//         showWarning("Fill the task");
+//     } else {
+//         clearWarning();
+//         Todo_list.push({
+//             task_list: todo_list_entered,
+//             datee: date_store_vari
+//         });
+
+//         entering_todolist.value = '';
+
+//         saveTodoListToLocalStorage();
+//         rendertodolist();
+
+//         noTasksMessage.textContent = "";
+//     }
+// }
+
+// function saveTodoListToLocalStorage() {
+//     localStorage.setItem('todoList', JSON.stringify(Todo_list));
+// }
+
+// document.querySelector('.js-add-todo-button')
+//     .addEventListener('click', () => {
+//         adding_the_todolist();
+//     });
+
+// document.querySelector('.inputed_value')
+//     .addEventListener('keydown', (event) => {
+//         if (event.key === 'Enter') {
+//             adding_the_todolist();
+//         }
+//     });
+
+// function addEditListeners() {
+//     const editButtons = document.querySelectorAll('.edit-button');
+
+//     editButtons.forEach((editButton, index) => {
+//         editButton.addEventListener('click', () => {
+//             // Get the corresponding task div
+//             const taskDiv = document.querySelectorAll('.display_result div')[index * 2];
+
+//             // Get the current task text
+//             const currentTaskText = taskDiv.textContent;
+
+//             // Create an input field for editing
+//             const inputField = document.createElement('input');
+//             inputField.value = currentTaskText;
+
+//             // Replace the task text with the input field
+//             taskDiv.textContent = '';
+//             taskDiv.appendChild(inputField);
+
+//             // Create a "Save" button
+//             const saveButton = document.createElement('button');
+//             saveButton.textContent = 'Save';
+
+//             // Add a click event for saving the edited task
+//             saveButton.addEventListener('click', () => {
+//                 const editedText = inputField.value;
+
+//                 // Update the task text and render the updated list
+//                 Todo_list[index].task_list = editedText;
+//                 saveTodoListToLocalStorage();
+//                 rendertodolist();
+//             });
+
+//             // Add the "Save" button after the input field
+//             taskDiv.appendChild(saveButton);
+//             saveButton.style.color = "#7fbc8c";
+//             saveButton.style.fontWeight = "700";
+//             saveButton.style.fontSize = "20px";
+//             saveButton.style.border = "none";
+//         });
+//     });
+// }
+
+// // imp code
+
+// document.getElementById('important-button').addEventListener('click', () => {
+
+//     const textElements = document.querySelectorAll('.display_result div:first-child');
+//     textElements.forEach((element) => {
+//         element.classList.toggle('important-text');
+
+//     });
+// });
+
+
